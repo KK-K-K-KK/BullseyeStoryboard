0d71e4960ec2f55c5dd86e17807185867ae2cffb
Kevin Kuan <kevinkuan0.app@outlook.com>
2023-02-13 19:21:01 -0800

    Add authors page - Add png profile picture - Add author description - Add new view controller class - Add UI in storyboard - Link UI button with dismiss function

---
+++ AuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorViewController.swift
+//  Bullseye storyboard
+//
+//  Created by Kevin Kuan on 2023/2/13.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        self.dismiss(animated: true)
+    }
+
+}
