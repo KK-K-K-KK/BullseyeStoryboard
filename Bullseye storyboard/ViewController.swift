da7f144680c17d214c21baca8a8543a3ac5db678
Kevin Kuan <kevinkuan0.app@outlook.com>
2023-02-13 16:43:33 -0800

    Add variables - round - target - modify alert

--- Bullseye storyboard/ViewController.swift
+++ Bullseye storyboard/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     var sliderValue : Int = 0
+    var roundValue : Int = 0
+    var targetValue : Int = 0
     
     @IBOutlet weak var slider: UISlider!
 
@@ -17,22 +19,33 @@ class ViewController: UIViewController {
         sliderValue = Int((slider.value * 100).rounded())
     }
     
+    func startNewRound() {
+        slider.value = 0.5
+        setSliderValue()
+        roundValue += 1
+        targetValue = Int.random(in: 1...100)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setSliderValue()
+        startNewRound()
     }
     
 
     @IBAction func showAlert() {
-        var alert = UIAlertController(title: "Boom!", message: "The slider value is \(sliderValue).", preferredStyle: .alert)
+        let message = "The slider value is \(sliderValue).\n The target value is \(targetValue).\n This is round \(roundValue)."
+        var alert = UIAlertController(title: "Boom!", message: message, preferredStyle: .alert)
         var alertAction = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(alertAction)
         self.present(alert, animated: true)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         setSliderValue()
     }
+    
+    
 }
 
