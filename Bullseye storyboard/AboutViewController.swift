42cc1c12b95a71aead02a9f5ac1702b8d83110ff
Kevin Kuan <kevinkuan0.app@outlook.com>
2023-02-13 18:38:44 -0800

    Add instructions screen - Create new view controller class for about screen - Textview UI with uneditable text - Button to close the view

---
+++ Bullseye storyboard/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  Bullseye storyboard
+//
+//  Created by Kevin Kuan on 2023/2/13.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        self.dismiss(animated: true)
+    }
+}
